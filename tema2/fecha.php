3c041037561136f2922344bf067e983b73612f93
”Smail” <smail.fahcha@educa.jcyl.es>
2023-10-03 16:23:54 +0000

    i/0310-1823

---
+++ tema2/fecha.php
@@ -0,0 +1,17 @@
+<?php
+echo time();
+echo "<br>";
+echo "zona que yine el servidor";
+echo "<br>";
+echo date_default_timezone_get();
+echo "<br> cambio";
+echo "<br>";
+date_default_timezone_set("Europe/Madrid");
+echo date_default_timezone_get();
+echo "<br> cambio";
+echo "<br>";
+echo date("r");
+echo "<br> en españa";
+echo "<br>";
+echo date("d/m/y H:i:s")
+?>
