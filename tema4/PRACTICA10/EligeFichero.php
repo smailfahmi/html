2852fd87c8dbeb06c960fdd9fe946cdd958be136
”Smail” <smail.fahcha@educa.jcyl.es>
2023-11-17 10:10:41 +0000

    c/1711-1110

--- tema4/PRACTICA10/EligeFichero.php
+++ tema4/PRACTICA10/EligeFichero.php
@@ -47,15 +47,105 @@
             }
             fclose($gestor);
         }
-
-        // Imprimir el array completo para verificar
+        echo '</table>';
+        $dom = new DOMDocument("1.0", "utf-8");
+        $raiz = $dom->appendChild($dom->createElement('alumnos'));
         echo '<pre>';
-        print_r($array);
+        // print_r($array);
+        foreach ($array as $key => $value) {
+            $alumno = $dom->createElement('alumno');
+            $raiz->appendChild($alumno);
+            $contador = 0;
+            foreach ($value as $elemento) {
+                switch ($contador) {
+                    case 0:
+                        $valor = $dom->createElement('nombre', $elemento);
+                        break;
+                    case 1:
+                        $valor = $dom->createElement('nota1', $elemento);
+                        break;
+                    case 2:
+                        $valor = $dom->createElement('nota2', $elemento);
+                        break;
+
+                    default:
+                        $valor = $dom->createElement('nota3', $elemento);
+                        break;
+                }
+                $alumno->appendChild($valor);
+                $contador++;
+            }
+        }
+        $dom->formatOutput = true;
+        $dom->save('notas.xml');
+
+
+
+        $dom->load('notas.xml');
+        // sabiendo lo qu ehay dentro y que todos los elementos son iguales
+        echo '<table> <tr>';
+
+        for ($i = 0; $i < 4; $i++) {
+            switch ($i) {
+                case 0:
+                    echo '<td> nombre </td>';
+                    break;
+                case 1:
+                    echo '<td> nota1 </td>';
+                    break;
+                case 2:
+                    echo '<td> nota2 </td>';
+                    break;
+
+                default:
+                    echo '<td> nota3 </td>';
+                    break;
+            }
+        }
+        echo '</tr>';
+        echo '<pre>';
+        foreach ($dom->getElementsByTagName('alumno') as $alumno) {
+            $nombre = $alumno->getElementsByTagName('nombre')->item(0)->nodeValue;
+            $nota1 = $alumno->getElementsByTagName('nota1')->item(0)->nodeValue;
+            $nota2 = $alumno->getElementsByTagName('nota2')->item(0)->nodeValue;
+            $nota3 = $alumno->getElementsByTagName('nota3')->item(0)->nodeValue;
+            echo "<tr> <td>$nombre</td> <td>$nota1</td> <td>$nota2</td> <td>$nota3</td>";
+        }
+
+        //esto solo funciona si el xml esta en linea es decir sin el codigo formateado 
+        // // print_r($dom);
+        // foreach ($dom->childNodes as $key) {
+        //     foreach ($key->childNodes as $key1) {
+        //         if ($key1->nodeType == 1) {
+        //             $nodo = $alumno->firstChild;
+        //             do {
+        //                 if ($key1->nodeType == 1) {
+        //                     echo "\n" . $nodo->tagName . ":" . $nodo->nodeValue;
+        //                 }
+        //             } while ($nodo = $nodo->nextSibling);
+        //         }
+        //         // echo "\nNombre: " . $key1->firstChild->nodeValue;
+        //         // echo "\nNombre: " . $key1->firstChild->firstChild->data;
+        //     }
+        // }
+
+        // echo '<pre>';
+        // foreach ($dom->childNodes as $key) {
+        //     if ($key->nodeType == 1) { // Verificar si el nodo es de tipo elemento (1)
+        //         foreach ($key->childNodes as $key1) {
+        //             if ($key1->nodeType == 1) { // Verificar si el nodo hijo es de tipo elemento (1)
+        //                 echo "\n" . $key1->tagName . ":" . $key1->nodeValue;
+        //             }
+        //         }
+        //     }
+        // }
+
+
+        # code...
+
+        // Iterar sobre los elementos 'alumno$alumno' e imprimir sus detalles
+
         ?>
-    </table>
-    <form action="" method="get">
-        <input type="submit" value="convertir" name="convertir">
-    </form>
 </body>
 
 </html>
