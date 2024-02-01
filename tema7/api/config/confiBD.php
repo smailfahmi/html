ceccafd051e0067909837a76a959acf0df3d44b2
”Smail” <smail.fahcha@educa.jcyl.es>
2024-02-01 19:40:20 +0000

    I/ALKJSF

--- tema7/api/config/confiBD.php
+++
@@ -1,6 +0,0 @@
-<?
-define('IP', '192.168.7.205');
-define('USER', 'api');
-define('PASS', 'api');
-define('BD', 'api');
-
--- tema7/consumirapi/confiApi.php
+++
@@ -1,2 +0,0 @@
-<?
-define("URI_API", 'http://192.168.7.205/tema7/api/index/');
